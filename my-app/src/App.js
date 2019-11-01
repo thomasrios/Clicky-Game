9a517b79271a8b3e98041051a9ff48fd1baef1c6
Thomas Rios <thomaserios@icloud.com>
2019-11-01 13:48:46 -0500

    Initial Files created and title working

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import Title from "./components/Title"
+
+class App extends Component {
+
+  // Map over this.state.friends and render a FriendCard component for each friend object
+  render() {
+    return (   
+     <div><Title /></div>
+    );
+  }
+}
+
+export default App;
---
+++ my-app/src/components/Title.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Title(props) {
+return (<div><h1>This is the clicky game</h1></div>)
+}
+
+export default Title;
+
+
