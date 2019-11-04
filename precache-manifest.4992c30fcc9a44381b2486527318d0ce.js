f90816e4c483e134b3c08c3c1d1aefc7c7ba05f5
Thomas Rios <thomaserios@icloud.com>
2019-11-04 09:42:07 -0600

    Updates

---
+++ precache-manifest.4992c30fcc9a44381b2486527318d0ce.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "554315ebf4c4e9ebaf1045bc1fb3c561",
+    "url": "/Clicky-Game/index.html"
+  },
+  {
+    "revision": "d8becd4ac1e7caef253b",
+    "url": "/Clicky-Game/static/css/main.85ecfa50.chunk.css"
+  },
+  {
+    "revision": "858337689f8457637e8e",
+    "url": "/Clicky-Game/static/js/2.05bcf8b3.chunk.js"
+  },
+  {
+    "revision": "d8becd4ac1e7caef253b",
+    "url": "/Clicky-Game/static/js/main.07922825.chunk.js"
+  },
+  {
+    "revision": "eabcff367a27d49b89d0",
+    "url": "/Clicky-Game/static/js/runtime-main.ca802268.js"
+  }
+]);
